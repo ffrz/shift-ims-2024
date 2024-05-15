0098d48d2c569effa556b09395e4d27b95a13740
Fahmi Fauzi Rahman <fahmifauzirahman@gmail.com>
2024-05-15 22:08:47 +0700

    print kartu servis 58mm

--- resources/views/admin/_layouts/print-invoice.blade.php
+++ resources/views/admin/_layouts/print-invoice.blade.php
@@ -5,21 +5,16 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>{{ $title }}</title>
-
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-
   <link rel="stylesheet" href="/plugins/fontawesome-free/css/all.min.css">
-
   <link rel="stylesheet" href="/dist/css/adminlte.min.css?v=3.2.0">
 </head>
 
-<body>
+<body style="width:58mm;">
   <div class="wrapper">
-    @yield('content')
-    <section class="invoice">
-
+    <section class="invoice"  style="width:100%;margin:2mm;">
+      @yield('content')
     </section>
-
   </div>
   <script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
   <script>
